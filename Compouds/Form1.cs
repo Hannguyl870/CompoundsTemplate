f4f481cbb55240b9578f3d8b294c6d8c3f7e150a
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-10-06 11:12:17 -0400

    code

--- Compouds/Form1.cs
+++ Compouds/Form1.cs
@@ -22,42 +22,100 @@ namespace Compounds
         private void hurricaneButton_Click(object sender, EventArgs e)
         {
             //get the inputted category
+           
+            string scale  =categoryOutput.Text;
 
             //check the inputted category with a switch block
+            switch(scale)
+            {
+                case "1":
+                    categoryOutput.Text = "119-153 km/h";
+                    break;
+                case "2":
+                    categoryOutput.Text = "154-177 km/h";
+                    break;
+                case "3":
+                    categoryOutput.Text = "179-209 km/h";
+                    break;
+                case "4":
+                    categoryOutput.Text = "210-149 km/h";
+                    break;
+                case "5":
+                    categoryOutput.Text = "greater than 249";
+                    break;
+                default:
+                    categoryOutput.Text = "only 1-5 are valid";
+                    break;
+            }
         }
 
         private void heartButton_Click(object sender, EventArgs e)
         {
             //set suit value
+            suit = "hearts";
 
             //set button colour to goldenrod, (other buttons black)
+            heartButton.BackColor = Color.Goldenrod;
+            diamondButton.BackColor = Color.Black;
+            clubButton.BackColor = Color.Black;
+            spadeButton.BackColor = Color.Black;
         }
 
         private void diamondButton_Click(object sender, EventArgs e)
         {
             //set suit value
 
+            suit = "diamonds";
+
             //set button colour to goldenrod, (other buttons black)
+            heartButton.BackColor = Color.Black;
+            diamondButton.BackColor = Color.Goldenrod;
+            clubButton.BackColor = Color.Black;
+            spadeButton.BackColor = Color.Black;
         }
 
         private void clubButton_Click(object sender, EventArgs e)
         {
             //set suit value
+            suit = "clubs";
+           
 
             //set button colour to goldenrod, (other buttons black)
+             heartButton.BackColor = Color.Black;
+            diamondButton.BackColor = Color.Black;
+            clubButton.BackColor = Color.Goldenrod;
+            spadeButton.BackColor = Color.Black;
         }
 
         private void spadeButton_Click(object sender, EventArgs e)
         {
             //set suit value
-
+            suit = "spades";
             //set button colour to goldenrod, (other buttons black)
+            heartButton.BackColor = Color.Black;
+            diamondButton.BackColor = Color.Black;
+            clubButton.BackColor = Color.Black;
+            spadeButton.BackColor = Color.Goldenrod;
         }
 
         private void orButton_Click(object sender, EventArgs e)
         {
             //use compound if statements to check what suit
             //has been selected and output the colour of the suit
+            if (suit == "hearts" || suit == "diamonds")
+            {
+                suitOutput.Text = "The suit is red";
+            }
+          
+            else if (suit == "spades"|| suit == "clubs") 
+            {
+                suitOutput.Text = "the suit is black";
+            }
+            else
+            {
+                suitOutput.Text = "select a suit";
+            }
+
         }
 
         private void thisButton_Click(object sender, EventArgs e)
